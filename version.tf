c9c0d04660187b62c81289551287cf43eb0140f5
dev-ahuja <ahuja.devender@gmail.com>
2023-09-11 08:41:42 +0530

    updated version.tf

--- version.tf
+++ version.tf
@@ -1,3 +1,20 @@
 terraform {
-    required_version= "1.4.6"
+    required_version= "1.4.6"  #major.minor.patch
+    required_providers {
+    aws = {
+        source  = "hashicorp/aws"
+        version = "~>5.0"
+    }
+  }
+}
+
+#"~>1.4" this is the most common way of defining the terraform version in your code.
+
+provider "aws" {
+  region = "us-east-1"
+  profile = "batch_sept"
+  assume_role {
+    role_arn = ""
+    session_name = "batch sept provisioning"
+  }
 }
